8cd261f630329223fc38602f29bf574d917132c7
anshushrivastava67 <78591207+anshushrivastava67@users.noreply.github.com>
2024-03-19 12:21:03 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -59,29 +59,24 @@ def run():
         return text
 
 
-    import os
-    
-    name='<h2>Resume Matching</h2>'
-    st.markdown(name, unsafe_allow_html=True)
-    
-    folder_path = r'C:\Users\anshu\Downloads\resume-20240212T152443Z-001\resume\Uploaded_Resumes'
-    job_description=pdf_reader(r'C:\Users\anshu\Downloads\resume-20240212T152443Z-001\resume\job_description.pdf')
-    similarities={}
-    for file_name in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, file_name)
-        if os.path.isfile(file_path):
-     
-            with open(file_path, 'r') as f:
-                
-                a=pdf_reader(file_path)
-                doc1 = nlp(job_description)
-                doc2 = nlp(a)
-               
-                similarity = doc1.similarity(doc2) 
-                similarities[file_name]=similarity
-    
-                html = '<html><head><style>.match-score{font-size:24px;font-weight:bold;color:#488f31;}.content{padding:20px;}</style></head><div class="content"><div><h4><b>Resume:</b>' + file_name + '</h4></div><p>' + a[:500] + '...</p><div class="match-score">Match Score: ' + str(int(similarity*100)) + '%</div></div></html>'
-                st.markdown(html, unsafe_allow_html=True)
+
+import os
+name='<h2>Resume Matching</h2>'
+st.markdown(name, unsafe_allow_html=True)
+job_description_file = st.file_uploader("Upload Job Description PDF", type=["pdf"])
+
+resume_files = st.file_uploader("Upload Resume PDFs", type=["pdf"], accept_multiple_files=True)
+similarities={}
+
+a=pdf_reader(file_path)
+doc1 = nlp(job_description)
+doc2 = nlp(a)
+
+similarity = doc1.similarity(doc2) 
+similarities[file_name]=similarity
+
+html = '<html><head><style>.match-score{font-size:24px;font-weight:bold;color:#488f31;}.content{padding:20px;}</style></head><div class="content"><div><h4><b>Resume:</b>' + file_name + '</h4></div><p>' + a[:500] + '...</p><div class="match-score">Match Score: ' + str(int(similarity*100)) + '%</div></div></html>'
+st.markdown(html, unsafe_allow_html=True)
     
     sorted_sim = []
     
